e4212d1a64117089255161f279e5036e95a7e628
Juan Reynaldo Escobar Miron <jrescobarmiron@gmail.com>
2018-03-06 17:41:51 -0600

    1.0.4

---
+++ PaymentResources/Classes/Base/hresources/paymentDemoIOS.h
@@ -0,0 +1,12 @@
+//
+//  paymentDemoIOS.h
+//  Pods
+//
+//  Created by Juan Reynaldo Escobar Miron on 06/03/18.
+//
+
+#ifndef paymentDemoIOS_h
+#define paymentDemoIOS_h
+
+
+#endif /* paymentDemoIOS_h */
